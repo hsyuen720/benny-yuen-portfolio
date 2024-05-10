e593b7f38717b2c72b42ed85420f540d28c36347
Benny Yuen <hsyuen720@gmail.com>
2024-05-10 16:05:06 +0200

    update lint staged

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,4 +1,4 @@
-module.exports = {
+export default {
   "*.ts?(x)": "tsc -p tsconfig.json --noEmit",
   "*.{js,jsx,ts,tsx}": "eslint --quiet --fix",
   "*.{json,md,mdx,yml}": "prettier --write --ignore-unknown",
