10670b5605eb246ef5efd86c0a4c0b35567b5e55
Benny Yuen <hsyuen720@gmail.com>
2025-10-03 19:33:45 +0800

    remove next lint

---
+++ eslint.config.mjs
@@ -0,0 +1,55 @@
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+
+import { FlatCompat } from "@eslint/eslintrc";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+});
+
+const eslintConfig = [
+  {
+    ignores: [
+      "**/node_modules/**",
+      "**/.next/**",
+      "**/out/**",
+      "**/build/**",
+      "**/dist/**",
+      "**/.git/**",
+      "**/coverage/**",
+      "**/.vercel/**",
+      "**/*.config.js",
+      "**/*.config.mjs",
+      "**/*.config.cjs",
+      "**/next-env.d.ts",
+      "**/.lintstagedrc.js",
+    ],
+  },
+  ...compat.extends(
+    "next/core-web-vitals",
+    "next/typescript",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:prettier/recommended",
+  ),
+  {
+    rules: {
+      "import/order": [
+        "error",
+        {
+          groups: ["builtin", "external", "internal", ["sibling", "parent"], "index", "unknown"],
+          "newlines-between": "always",
+          alphabetize: {
+            order: "asc",
+            caseInsensitive: true,
+          },
+        },
+      ],
+      "@typescript-eslint/no-unused-vars": "warn",
+    },
+  },
+];
+
+export default eslintConfig;
