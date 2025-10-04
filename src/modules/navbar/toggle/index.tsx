d99fbecd42632cecbaa5e51521254c51aa7b7b20
Benny Yuen <hsyuen720@gmail.com>
2025-10-04 20:19:55 +0800

    fix hamburger

--- src/modules/navbar/toggle/index.tsx
+++ src/modules/navbar/toggle/index.tsx
@@ -16,19 +16,18 @@ const Toggle = (props: ToggleProps) => {
       aria-controls="navbar-menu"
       aria-expanded={isOpen}
       onClick={onClick}
+      type="button"
+      aria-label={isOpen ? "Close navigation menu" : "Open navigation menu"}
     >
-      <svg className={styles.hamburger} viewBox="0 0 100 100">
-        {Array.from({ length: 3 }).map((_, index) => (
-          <rect
-            className={styles.line}
-            key={index}
-            width={80}
-            height={10}
-            x={10}
-            y={25 + 20 * index}
-            rx={5}
-          />
-        ))}
+      <svg
+        className={styles.hamburger}
+        viewBox="0 0 100 100"
+        xmlns="http://www.w3.org/2000/svg"
+        preserveAspectRatio="xMidYMid meet"
+      >
+        <rect className={styles.line} width={80} height={10} x={10} y={25} rx={5} />
+        <rect className={styles.line} width={80} height={10} x={10} y={45} rx={5} />
+        <rect className={styles.line} width={80} height={10} x={10} y={65} rx={5} />
       </svg>
     </button>
   );
